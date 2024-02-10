1c141944868ff58456d9b09be44cd0fd95454ba3
LALITH2810 <127330591+LALITH2810@users.noreply.github.com>
2024-02-10 15:06:45 +0530

    Add files via upload

--- t1.py
+++ t1.py
@@ -1,15 +1,20 @@
-def fibonacci(n):
+def fibonacci(n, memo={}):
     if n <= 1:
         return n
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+    elif n not in memo:
+        memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+    return memo[n]
 
 def print_fibonacci_series(n):
-    for i in range(n):
+    for i in range(1, n + 1):
         print(fibonacci(i), end=" ")
 
 if __name__ == "__main__":
     n = int(input("Enter the value of n: "))
-    print(f"The {n}th Fibonacci number is: {fibonacci(n)}")
-    print(f"The Fibonacci series up to the {n}th number is:", end=" ")
-    print_fibonacci_series(n)
+    
+    if n < 0:
+        print("Please enter a non-negative integer.")
+    else:
+        print(f"The {n}th Fibonacci number is: {fibonacci(n)}")
+        print(f"The Fibonacci series up to the {n}th number is:", end=" ")
+        print_fibonacci_series(n)
